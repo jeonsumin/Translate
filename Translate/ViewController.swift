a9c1e2a3c71bda89bcfe81a3541e6dacc5bc8d19
jeonsumin <soomis7@naver.com>
2022-03-10 22:20:55 +0900

    Initial Commit

---
+++ Translate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by Terry on 2022/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
